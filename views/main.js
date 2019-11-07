04889f962dffaf51fec6eb7ca239e8f625a6b6a3
heshrian <kamburovviktor@gmail.com>
2019-11-07 14:48:32 +0100

    edit():main.js

--- views/main.js
+++ views/main.js
@@ -1,29 +1,21 @@
 'use strict'
 
-// const top = document.querySelector('.topList');
+const top = document.querySelector('.topList');
 const allMen = document.querySelector('.allMentors')
 
 
 window.onload = function loadData() {
-  fetch('/top')
+  console.log('asd')
+  fetch('top')
     .then(response => response.json())
     .then(response => {
       response.forEach(element => {
-        // let name = document.createElement('span');
-        // let ranking = document.createElement('span');
-        // let average = document.createElement('span');
-        // name.textContent = element.name
-        // ranking.textContent = element.rank
-        // average.textContent = element.average
         let liElem = document.createElement('li');
         liElem.textContent = `${element.name} ${element.rank} ${element.average}`
-        top.appendChild(liElem)
+        topMen.appendChild(liElem)
       });
     })
-}
-
-window.onload = function loadData() {
-  fetch('http://localhost:3000/mentors')
+    fetch('http://localhost:3000/mentors')
     .then(response => response.json())
     .then(response => {
       response.forEach(element => {
