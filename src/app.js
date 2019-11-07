e9f315a1f354948dd6eb901eab1d4f1e3f0ec0f0
heshrian <kamburovviktor@gmail.com>
2019-11-07 17:02:56 +0100

    edit():mentor_rating

--- src/app.js
+++ src/app.js
@@ -10,54 +10,54 @@ app.set('view engine', 'ejs');
 app.use(express.static('./views'));
 app.use(express.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile('/index.html')
 });
 
-app.get('/top', function(req, res) {
+app.get('/top', function (req, res) {
   askDatabase('SELECT name, explanation, knowledge, helpfulness, mark_sum FROM mentors', [])
-  .then((result) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(rank(result));
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.sendStatus(500);
-  })
+    .then((result) => {
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(rank(result));
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.sendStatus(500);
+    })
 });
 
-app.get('/mentors', function(req, res) {
+app.get('/mentors', function (req, res) {
   askDatabase('SELECT id, name FROM mentors', [])
-  .then((result) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(result);
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.sendStatus(500);
-  });
+    .then((result) => {
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.sendStatus(500);
+    });
 });
 
-app.get('/profile/:id', function(req, res) {
-  res.sendFile('/mentor_rating.html');
+app.get('/profile/:id', function (req, res) {
+  res.sendFile('mentor_rating.html', { root: path.join(__dirname, '../views') })
 })
 
-app.get('/mentor/:id', function(req, res) {
+app.get('/mentor/:id', function (req, res) {
   askDatabase('SELECT * FROM mentors WHERE id = ?', [req.params.id])
-  .then((result) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(editMentorData(result[0]));
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.sendStatus(500);
-  });
+    .then((result) => {
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(editMentorData(result[0]));
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.sendStatus(500);
+    });
 });
 
-app.put('/mentor/:id', function(req, res) {
+app.put('/mentor/:id', function (req, res) {
   askDatabase(`UPDATE mentors SET 
     explanation = explanation + ?,
     knowledge = knowledge + ?,
@@ -70,18 +70,18 @@ app.put('/mentor/:id', function(req, res) {
       req.body.helpfulness,
       req.params.id
     ])
-  .then((result) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send({ message: 'Thanks for your feedback!' });
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.sendStatus(500);
-  });
+    .then((result) => {
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send({ message: 'Thanks for your feedback!' });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.sendStatus(500);
+    });
 });
 
-app.post('/new', function(req, res) {
+app.post('/new', function (req, res) {
   askDatabase(`INSERT INTO mentors
   (name,
   class,
@@ -94,26 +94,26 @@ app.post('/new', function(req, res) {
   helpfulness,
   mark_sum)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, 1)`,
-  [
-    req.body.name,
-    req.body.class,
-    req.body.fullstack,
-    req.body.backend,
-    req.body.devops,
-    req.body.embedded,
-    req.body.explanation,
-    req.body.knowledge,
-    req.body.helpfulness
-  ])
-  .then((result) => {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send({ message: 'Thanks for the update!' });
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.sendStatus(500);
-  });
+    [
+      req.body.name,
+      req.body.class,
+      req.body.fullstack,
+      req.body.backend,
+      req.body.devops,
+      req.body.embedded,
+      req.body.explanation,
+      req.body.knowledge,
+      req.body.helpfulness
+    ])
+    .then((result) => {
+      res.status(200);
+      res.setHeader('Content-Type', 'application/json');
+      res.send({ message: 'Thanks for the update!' });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = app;
--- views/mentor_rating.js
+++ views/mentor_rating.js
@@ -1,13 +1,15 @@
-let span = document.querySelector('name');
-let stack = document.querySelector('stack');
-let theClass = document.querySelector('theClass');
-let expl = document.querySelector('expl');
-let know = document.querySelector('know');
-let help = document.querySelector('help');
 
-// get method
+let span = document.querySelector('.name');
+let stack = document.querySelector('.stack');
+let theClass = document.querySelector('.theClass');
+let expl = document.querySelector('.expl');
+let know = document.querySelector('.know');
+let help = document.querySelector('.help');
+
+
 window.onload = function loadData() {
-  fetch(`http://localhost:3000/mentor/:${id}`)
+  console.log(window.location.pathname)
+  fetch(`http://localhost:3000/profile/:id`)
     .then(response => response.json())
     .then(response => {
       span.textContent = response.name;
